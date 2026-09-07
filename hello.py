4cce43f3953231c0f641645bfe03925cb85f4bc9
ramaspg76-rgb <ramaspg76@gmail.com>
2026-09-07 11:18:40 +0700

    yaayayyayay

---
+++ hello.py
@@ -0,0 +1,6 @@
+import sys
+print("Praktek Pemrograman Terapan - Pertemuan 1")
+print("Nama :", "Moh Awalul Ramadhan")
+print("NRP :", "33132501011")
+print("Python:", sys.version)
+print("pesut kali")
